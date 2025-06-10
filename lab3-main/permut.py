194e3ba67d737db49ea17e8d34cbc9b81f91ed40
Vilen-web <v_gridassov@kbtu.kz>
2025-06-10 11:20:47 +0500

    Delete lab3-main/permut.py

--- lab3-main/permut.py
+++
@@ -1,8 +0,0 @@
-import itertools
-def print_permutations():
-    user_input = input()
-    permutations = itertools.permutations(user_input)
-    for perm in permutations:
-        print(''.join(perm))
-
-print_permutations()
