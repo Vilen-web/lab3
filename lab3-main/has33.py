172326c5ee0519363fbefcc02846205abeaec5ce
Vilen-web <v_gridassov@kbtu.kz>
2025-06-10 11:15:18 +0500

    1.1.py

--- lab3-main/has33.py
+++ lab3-main/has33.py
@@ -1,10 +1,2 @@
-def has33(nums):
-    for i in range(len(nums) - 1):
-        if nums[i] == 3 and nums[i + 1] == 3:
-            return True
-    return False
-
-three = input()
-numbers = [int(num) for num in three.split()]
-result = has33(numbers)
-print(result)
+def grams_to_ounces(grams):
+    return 28.3495231 * grams
