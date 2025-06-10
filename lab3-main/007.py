36914f71f5a477906a53cda27efb4db1e13c14ee
Vilen-web <v_gridassov@kbtu.kz>
2025-06-10 11:26:02 +0500

    Delete lab3-main/007.py

--- lab3-main/007.py
+++
@@ -1,9 +0,0 @@
-class StringManipulator:
-    def __init__(self):
-        self.input_string = ""
-
-    def getString(self):
-        self.input_string = input("Enter a string: ")
-
-    def printString(self):
-        print(self.input_string.upper())
