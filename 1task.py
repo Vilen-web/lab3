2cf7af5f40ac9192bde8344e81e7a9e665bc8c56
Vilen-web <v_gridassov@kbtu.kz>
2025-06-10 11:28:30 +0500

    Add files via upload

---
+++ 1task.py
@@ -0,0 +1,79 @@
+class StringManipulator:
+    def __init__(self):
+        self.input_string = ""
+
+    def getString(self):
+        self.input_string = input("Enter a string: ")
+
+    def printString(self):
+        print(self.input_string.upper())
+
+
+class Shape:
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+    
+    def area(self):
+        return self.length ** 2
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        return self.length * self.width
+
+
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def show(self):
+        print(f"Point coordinates: ({self.x}, {self.y})")
+    
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+    
+    def dist(self, other_point):
+        return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+
+
+
+class Account:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance: {self.balance}")
+    
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+        else:
+            print("Insufficient funds!")
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numbers = [10, 13, 17, 20, 23, 25, 29]
+prime_numbers = list(filter(lambda x: is_prime(x), numbers))
+print(f"Prime numbers: {prime_numbers}")
